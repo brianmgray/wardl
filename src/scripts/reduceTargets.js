0c52a81a18d20ad0421f2cfc61aefd1b6c6d99e5
Brian Gray <brian.gray@pnc.com>
2022-03-08 07:17:07 -0500

    Update reduceTargets to check index size, update targets

--- src/scripts/reduceTargets.js
+++ src/scripts/reduceTargets.js
@@ -1,29 +1,37 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
+const mnemonist = require('mnemonist');
+const levenshteinLte1 = require('levenshtein-lte1')
+
 const targets = require("./../data/targets.original.json")
 
 // script constants
 const DIR_OUTPUT = './../data'
 const FILE_OUTPUT = DIR_OUTPUT + "/targets.json";
-const MAX_RARITY_WORD = "murky";
+// const MAX_RARITY_DEPTH = 14501; //=> 1436 potential targets
+const MAX_RARITY_DEPTH = 16500; //=> 1144
 const WORD_LENGTH = 5;
 
 /**
  * Script runner
  */
 class Runner {
-
   go() {
     console.log("Running reduceTargets");
     console.log(`Found ${Object.keys(targets).length} potential targets`);
 
     let updated = targets
-      .slice(0, targets.indexOf(MAX_RARITY_WORD) + 1)
-      .filter((word) => word.length === WORD_LENGTH && word !== "*****")
+      .slice(0, MAX_RARITY_DEPTH)
+      .filter(word => word.length === WORD_LENGTH && word !== "*****")
 
-    this.writeOutput(updated);
-    console.log(`Output ${Object.keys(updated).length} potential targets`);
+    console.log(`Updated list has ${updated.length} words`);
+
+    // now that we've reduced the targets, keep only words with more than 1 permutation
+    let index = new mnemonist.PassjoinIndex.from(updated, levenshteinLte1, 1);
+    let final = updated.filter(word => index.search(word).size > 1 )  
+    console.log(`Output ${final.length} potential targets`);
+    this.writeOutput(final);
   }
 
   writeOutput(jsonContent) {
