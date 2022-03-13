e80793524120048644e289fca3d3857b1f587130
Brian Gray <brian.gray@pnc.com>
2022-03-12 22:43:11 -0500

    Updated About page, closes #2

--- src/About.tsx
+++ src/About.tsx
@@ -20,6 +20,9 @@ export function About() {
         Each guess must be a valid five-letter word. After each guess, the tiles 
         will show you how close your guess was to the target.
       </p>
+      <p>
+        <strong>Example:</strong>
+      </p>
       <table
         className="Game-rows"
         aria-label="Wordl Review">
@@ -28,47 +31,37 @@ export function About() {
           rowState={RowState.LockedIn}
           wordLength={5}
           cluedLetters={[
-            { clue: Clue.Absent, letter: "w" },
-            { clue: Clue.Correct, letter: "a" },
-            { clue: Clue.Absent, letter: "r" },
-            { clue: Clue.Elsewhere, letter: "d" },
-            { clue: Clue.Absent, letter: "l" },
+            { clue: Clue.Correct, letter: "s" },
+            { clue: Clue.Absent, letter: "p" },
+            { clue: Clue.Elsewhere, letter: "e" },
+            { clue: Clue.Absent, letter: "c" },
+            { clue: Clue.Absent, letter: "k" },
           ]}
-          showTargets={false}
+          showTargets={true}
+          target=""
         />
         </tbody>
       </table>
       <p>
-        <strong>Example:</strong>
         <ul>
-          <li><b>W</b> and <b>L</b> aren't in the target word at all.</li>
+          <li><b>P</b>, <b>C</b> and <b>K</b> aren't in the target word at all.</li>
           <li>
-            <b className={"green-bg"}>A</b> is correct! The second letter is{" "}
-            <b className={"green-bg"}>A</b>
+            <b className={"green-bg"}>S</b> is correct! The first letter is{" "}
+            <b className={"green-bg"}>S</b>
             .<br />
-            <strong>(There may still be a second A in the word.)</strong>
+            (There could also be a second S in the word.)
           </li>
           <li>
-            <b className={"yellow-bg"}>D</b> occurs <em>elsewhere</em> in the target word.
-            <br />
-            <strong>(Perhaps more than once. 🤔)</strong>
+            <b className={"yellow-bg"}>E</b> occurs <em>elsewhere</em> in the target word.<br />
+            (Perhaps more than once 🤔)
           </li>
         </ul> 
       </p>
       <hr />
       <h2>Wardl</h2>
       <p>
-        In <strong>Wardl</strong>, on every incorrect guess, the target word changes one 
-        letter at a time. 
-        <ul>
-          <li>Each target is always a valid 5-letter word.</li>
-          <li>The color-coded tiles all update for the new target.</li>
-          <li>The changed letter gets highlighted (COMING SOON).</li>
-          <li>If no one-letter change makes a valid word, the word remains the same.</li>
-        </ul>
-      </p>
-      <p>
-        Let's continue our example!
+        In <strong>Wardl</strong>, the target word changes by one letter on every incorrect guess. Each
+        change always results in a valid 5 letter word. Let's look at an example game.
       </p>
       <table
         className="Game-rows"
@@ -78,52 +71,60 @@ export function About() {
           rowState={RowState.LockedIn}
           wordLength={5}
           cluedLetters={[
-            { clue: Clue.Absent, letter: "w" },
-            { clue: Clue.Correct, letter: "a" },
-            { clue: Clue.Absent, letter: "r" },
-            { clue: Clue.Absent, letter: "d" },
-            { clue: Clue.Elsewhere, letter: "l" },
-          ]}
-          showTargets={true}
-          target={'daisy'}
-        />
-        <Row
-          rowState={RowState.LockedIn}
-          wordLength={5}
-          cluedLetters={[
-            { clue: Clue.Correct, letter: "d" },
-            { clue: Clue.Correct, letter: "a" },
-            { clue: Clue.Correct, letter: "i" },
             { clue: Clue.Absent, letter: "s" },
-            { clue: Clue.Correct, letter: "y" },
+            { clue: Clue.Absent, letter: "p" },
+            { clue: Clue.Elsewhere, letter: "e" },
+            { clue: Clue.Absent, letter: "c" },
+            { clue: Clue.Absent, letter: "k" },
           ]}
           showTargets={true}
-          target={'daily'}
+          target={'shall'}
         />
         <Row
           rowState={RowState.LockedIn}
           wordLength={5}
           cluedLetters={[
-            { clue: Clue.Correct, letter: "g" },
-            { clue: Clue.Correct, letter: "a" },
-            { clue: Clue.Correct, letter: "i" },
+            { clue: Clue.Absent, letter: "s" },
+            { clue: Clue.Correct, letter: "h" },
+            { clue: Clue.Absent, letter: "e" },
             { clue: Clue.Correct, letter: "l" },
-            { clue: Clue.Correct, letter: "y" },
+            { clue: Clue.Absent, letter: "l" },
           ]}
           showTargets={true}
-          target={'gaily'}
+          target={'shale'}
+        />
+        <Row
+          rowState={RowState.LockedIn}
+          wordLength={5}
+          cluedLetters={[
+            { clue: Clue.Correct, letter: "w" },
+            { clue: Clue.Correct, letter: "h" },
+            { clue: Clue.Correct, letter: "a" },
+            { clue: Clue.Correct, letter: "l" },
+            { clue: Clue.Correct, letter: "e" },
+          ]}
+          showTargets={true}
+          target={'whale'}
         />
         </tbody>
       </table>
+      <p>
+        NOTE: You don't see the targets until the game ends. 
+      </p>
+      <p>
+        The clues always relate to the current target. For example, when the target changed from 
+        "shale" to "whale", the <b className={"green-bg"}>S</b> became an <b>S</b> because "S" was 
+        no longer in the correct place. 
+      </p>
+      <hr />
       <p>
         Report issues{" "}
         <a href="https://github.com/brianmgray/wardl/issues">here</a>, or tweet{" "}
         <a href="https://twitter.com/briandoesimprov">@briandoesimprov</a>.
       </p>
       <p>
-        This game will be free and ad-free forever,
-        <br />
-        but you can <a href="https://www.paypal.com/paypalme/bmgray84">buy me a coffee</a> if
+        This game will be free and ad-free forever, 
+        but you can <a href="https://www.paypal.com/paypalme/bmgray84">buy&nbsp;me&nbsp;a&nbsp;coffee</a> if
         you'd like.
       </p>
     </div>
