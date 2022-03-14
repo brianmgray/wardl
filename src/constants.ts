4381af3fab4f042aa7a64987c4860b97bffda062
Brian Gray <brian.gray@pnc.com>
2022-03-13 21:58:59 -0400

    Disable debugging in prod

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@
  */
 
 export const Constants = {
-  ENABLE_DEBUG_TARGETS: false,
+  ENABLE_DEBUG_TARGETS: window.location.hostname === "localhost",
   MAX_GUESSES: 6,
   WORD_LENGTH: 5,
   WARDLE_URL: "wardl.co"
