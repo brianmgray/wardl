5f6a3e6cce3188123401a0789188f3341c9229d3
Brian Gray <brianmgray@gmail.com>
2022-04-03 22:52:34 -0400

    Add share logic for guesses fixes issue #8 (#9)
    Co-authored-by: Brian Gray <brian.gray@pnc.com>

--- src/__tests__/clue.test.ts
+++ src/__tests__/clue.test.ts
@@ -1,4 +1,4 @@
-import {Clue, CluedLetter, clue} from "../clue";
+import {Clue, CluedLetter, clue, clueToEmoji} from "../clue";
 
 describe('clue', () => {
 
@@ -6,7 +6,7 @@ describe('clue', () => {
         let word = "speck"
         let target = "shale"
         let historical = "shall"
-        let letters:CluedLetter[] = clue(word, target, historical);
+        let letters:CluedLetter[] = clue(word, target, target, historical);
         console.log(letters);
         expect(letters[0]).toEqual(expect.objectContaining({ letter: 's', clue: Clue.Correct }));
         expect(letters[1]).toEqual(expect.objectContaining({ letter: 'p', clue: Clue.Absent }));
@@ -19,13 +19,42 @@ describe('clue', () => {
         let word = "speck"
         let target = "shale"
         let historical = "shall"
-        let letters:CluedLetter[] = clue(word, target, historical);
+        let letters:CluedLetter[] = clue(word, target, target, historical);
         console.log(letters);
         expect(letters[0]).toEqual(expect.objectContaining({ letter: 's', changed: false }));
         expect(letters[1]).toEqual(expect.objectContaining({ letter: 'p', changed: false }));
         expect(letters[2]).toEqual(expect.objectContaining({ letter: 'e', changed: false }));
         expect(letters[3]).toEqual(expect.objectContaining({ letter: 'c', changed: false }));
         expect(letters[4]).toEqual(expect.objectContaining({ letter: 'k', changed: true }));
+    });
+
+});
+
+
+describe('clueToEmoji', () => {
+
+    test('colorblind guess', () => {
+        let cl: CluedLetter = {letter: "A", clue: Clue.Absent, guessClue: Clue.Correct};
+        let e = clueToEmoji(cl, true);
+        expect(e).toEqual("🟠");
+    });
+
+    test('colorblind clue', () => {
+        let cl: CluedLetter = {letter: "A", clue: Clue.Elsewhere, guessClue: Clue.Correct};
+        let e = clueToEmoji(cl, true);
+        expect(e).toEqual("🟦");
+    });
+
+    test('guess', () => {
+        let cl: CluedLetter = {letter: "A", clue: Clue.Absent, guessClue: Clue.Elsewhere};
+        let e = clueToEmoji(cl, false);
+        expect(e).toEqual("🟡");
+    });
+
+    test('clue', () => {
+        let cl: CluedLetter = {letter: "A", clue: Clue.Correct, guessClue: Clue.Absent};
+        let e = clueToEmoji(cl, false);
+        expect(e).toEqual("🟩");
     });
 
 });
